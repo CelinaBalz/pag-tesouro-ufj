0052b0430c243444e80d49fc66c3746defba8e31
Celina Balz <celina.balz@ufj.edu.br>
2023-12-12 15:54:33 -0300

    feat formatando input de nome para receber somente letras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,15 @@ export default function Home() {
     value:'',
   })
   
+{/* ------------------- FORMATANDO INPUT NOME PARA RECEBER APENAS LETRAS  ----------------------------------------------------------------------- */}
+  const handleChangeName = (event) => {
+    const inputValue = event.target.value;
+    const formattedValue = inputValue.replace(/[^a-zA-Z\s]/g, '');
+    setName(formattedValue);
+  }
+
+
+
 {/* ------------------- FORMATANDO INPUT DO VALOR EM REAIS BR  ----------------------------------------------------------------------- */}
   const formatCurrency = (input) => {
     const cleanValue = input.replace(/[^\d]/g, '');
@@ -111,7 +120,7 @@ export default function Home() {
         </div>
         
         <div >
-          <Image src={faixa}  className=' w-[100vw] lg:h-[15vh]' alt="" />
+          <Image src={faixa}  className=' w-[100vw] lg:h-[12vh] opacity-60' alt="" />
         </div>
 
         <form onSubmit={handleSignupForm} className='relative lg:h-[90vh] md:items-center pt-[44px] pb-36 px-5 flex flex-col gap-3  items-start'>
@@ -192,10 +201,11 @@ export default function Home() {
                   className='lg:w-[28.75rem] lg:text-xl mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-noneinvalid:border-pink-500 invalid:text-pink-60 focus:invalid:border-pink-500 focus:invalid:ring-pink-500' 
                   name="nome" 
                   type="text" 
+                  id='nome'
                   placeholder="Nome completo" 
                   required
                   value={name}
-                  onChange={(event) => setName(event.target.value)}
+                  onChange={handleChangeName}
                 ></input> 
               </div>
 {/* ------------------- INPUT VALOR ----------------------------------------------------------------------- */}
@@ -227,10 +237,8 @@ export default function Home() {
               <button className='lg:self-end lg:w-56 lg:h-10 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-105 hover:bg-azul-200 duration-300 shadow-lg shadow-blue-100 self-center mt-5 w-36 h-8 rounded-full bg-azul-500  text-blue-50 font-bold ' type="submit">Enviar</button>
             </div> 
           </div>
-        </form>
-        
-
-        
+        </form>     
+       
         <footer className='border-t-2 flex flex-col md:flex-row md:justify-around items-center'>
           <div className='py-4 flex flex-col place-items-center'>
             <h3 className='font-bold pb-3'>Contato</h3>
