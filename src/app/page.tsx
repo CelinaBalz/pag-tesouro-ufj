f75c418c17e4d504d61195917dbecdeb179dfd73
Celina Balz <celina.balz@ufj.edu.br>
2023-12-12 14:39:40 -0300

    feat alteração no style do h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
       <div className="lg:text-xl font-body z-10  w-full items-center justify-between text-sm ">
     
         <div className='bg-azul-900 w-[100vw]'>
-          <h1 className="lg:text-2xl text-lg p-2 px-2 text-white ">Universidade Federal de Jatai</h1>
+          <h1 className="text-sm p-3 px-5 text-slate-300 tracking-widest	">Universidade Federal de Jatai</h1>
         </div>
         
         <div >
@@ -98,7 +98,7 @@ export default function Home() {
         </div>
 
         <form onSubmit={handleSignupForm} className='relative lg:h-[90vh] md:items-center pt-[44px] pb-36 px-5 flex flex-col gap-3  items-start'>
-          <div className='xl:w-[50vw] lg:w-[90vw]  lg:pb-16'>
+          <div className='xl:w-[67vw] lg:w-[90vw]  lg:pb-16'>
             <h1 className='lg:text-2xl text-lg  font-bold self'>Preencha seus dados</h1>
             <p>Preencha seus dados para prosseguir com o pagamento de GRU</p>
           </div>
@@ -152,11 +152,11 @@ export default function Home() {
               </label>
             </div>
 {/* ------------------- INPUT CPF ----------------------------------------------------------------------- */}
-            <div className='flex flex-col gap-5'>
+            <div className='flex flex-col gap-10'>
               <div className='flex flex-col '>
                 <label className='font-bold' htmlFor="cpf">CPF ou CNPJ do Contribuinte</label>
                 <input 
-                className='lg:w-[28.75rem] lg:text-xl mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500' 
+                className='lg:w-[28.75rem] lg:text-xl mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-noneinvalid:border-pink-500 invalid:text-pink-60 focus:invalid:border-pink-500 focus:invalid:ring-pink-500' 
                 name="cpf" 
                 type="text" 
                 placeholder="CPF completo" 
@@ -169,7 +169,7 @@ export default function Home() {
               </div>
              </div>
 {/* ------------------- INPUT NOME ----------------------------------------------------------------------- */}
-              <div className='flex flex-col gap-3'>
+              <div className='flex flex-col '>
                 <label className='font-bold' htmlFor="nome">Nome completo do contribuinte</label>
                 <input 
                   className='lg:w-[28.75rem] lg:text-xl mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-noneinvalid:border-pink-500 invalid:text-pink-60 focus:invalid:border-pink-500 focus:invalid:ring-pink-500' 
@@ -182,10 +182,10 @@ export default function Home() {
                 ></input> 
               </div>
 {/* ------------------- INPUT VALOR ----------------------------------------------------------------------- */}
-              <div className='flex flex-col gap-3'>
+              <div className='flex flex-col '>
               <label  className='font-bold' htmlFor="valor">Valor</label>
                 <input 
-                  className='lg:w-[28.75rem] lg:text-xl  mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500' 
+                  className='lg:w-[28.75rem] lg:text-xl mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-noneinvalid:border-pink-500 invalid:text-pink-60 focus:invalid:border-pink-500 focus:invalid:ring-pink-500' 
                   name="valor" 
                   type="number" 
                   placeholder="Valor" 
