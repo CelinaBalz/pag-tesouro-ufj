3f41a1e1c2c1b3257d9bf8e3195bfb0f817da6eb
Celina Balz <celina.balz@ufj.edu.br>
2023-12-15 14:27:18 -0300

    refactor Transformando modal em componente react

---
+++ src/app/components/Modal.tsx
@@ -0,0 +1,23 @@
+// Modal.js
+import React from 'react';
+
+const Modal = ({ isVisible, handleCloseModal, generatedLink }) => {
+  return isVisible && (
+    <div className='m-auto top-0 left-0 bottom-0 right-0 absolute bg-slate-900 bg-opacity-25 w-[100vw] h-[100vw]'>
+            <div className='flex flex-col  items-center rounded-xl  justify-center m-auto top-0 left-0 bottom-0 right-0 absolute bg-slate-50 w-[60vw] h-[30vh] shadow-2xl shadow-slate-300 '>
+        <button onClick={handleCloseModal} className='self-end px-5 font-bold text-2xl hover:text-gray-500'>
+            X
+            </button>
+        <div className='flex flex-col rounded-full'>
+          <h3 className='lg:text-2xl text-lg  font-bold self '>Link do GRU gerado com sucesso</h3>
+          <p>Clique no botão criado a seguir para prosseguir</p>
+          <a href={generatedLink} className=' rounded-full bg-azul-500  text-blue-50 font-bold py-2 my-5 text-center shadow-md hover:shadow-azul-200 hover:bg-blue-700 hover:ease-in-out cursor-pointer'>
+            Ir para o Pagamento
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import './globals.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Modal from './components/Modal'
 
 export default function Home() {
   const [cpf, setCPF] = useState('')
@@ -292,26 +293,9 @@ export default function Home() {
             </div> 
           </div>
         </form>     
-       
         <Footer></Footer>
-{/* ------------------- MODAL   ----------------------------------------------------------------------- */}
-        {modalVisible &&(
-          <div className=' m-auto top-0 left-0 bottom-0 right-0 absolute bg-slate-900 bg-opacity-25 w-[100vw] h-[100vw]'>
-            <div className='flex flex-col  items-center rounded-xl  justify-center m-auto top-0 left-0 bottom-0 right-0 absolute bg-slate-50 w-[60vw] h-[30vh] shadow-2xl shadow-slate-300 '>
-               <button onClick={handleCloseModal} className='self-end px-5 font-bold text-2xl hover:text-gray-500'>
-                 X
-                </button>
-            <div className='flex flex-col rounded-full '>
-                <h3 className='lg:text-2xl text-lg  font-bold self '>Link do GRU gerado com sucesso </h3>
-                <p>Clique no botão criado à seguir para prosseguir</p>
-                <a href={generatedLink} className=' rounded-full bg-azul-500  text-blue-50 font-bold py-2 my-5 text-center shadow-md hover:shadow-azul-200 hover:bg-blue-700 hover:ease-in-out cursor-pointer '>
-                Ir para o Pagamento
-                </a>
-             </div>
-            </div>
-          </div>
-        )}
-{/* ------------------- MODAL  FIM ----------------------------------------------------------------------- */}
+        <Modal isVisible={modalVisible} handleCloseModal={handleCloseModal} generatedLink={generatedLink} />
+       
 
       </div>
     </main>
